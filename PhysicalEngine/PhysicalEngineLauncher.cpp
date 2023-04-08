fe48d8767128f7cf0fbbc668774feddf3f8a13f7
Im-Rises <quentin-morel88@hotmail.com>
2023-04-08 00:20:37 -0400

    wip

--- PhysicalEngine/PhysicalEngineLauncher.cpp
+++ PhysicalEngine/PhysicalEngineLauncher.cpp
@@ -36,7 +36,7 @@
 #include <emscripten/html5.h>
 #endif
 
-static void glfw_error_callback(int error, const char* description) {
+static void glfw_error_callback(int error, const char *description) {
     fprintf(stderr, "Glfw Error %d: %s\n", error, description);
 }
 
@@ -46,7 +46,7 @@ PhysicalEngineLauncher::PhysicalEngineLauncher() {
     if (glfwInit() == 0)
         exit(1);
 
-        // Decide GL+GLSL versions
+    // Decide GL+GLSL versions
 #if defined(IMGUI_IMPL_OPENGL_ES2)
     const char* glsl_version = "#version 300 es";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -59,7 +59,7 @@ PhysicalEngineLauncher::PhysicalEngineLauncher() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // 3.2+ only
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);           // Required on Mac
 #else
-    const char* glsl_version = "#version 330";
+    const char *glsl_version = "#version 330";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // 3.2+ only
@@ -89,8 +89,8 @@ PhysicalEngineLauncher::PhysicalEngineLauncher() {
 
 #ifndef __EMSCRIPTEN__
     // Center window
-    GLFWmonitor* monitor = glfwGetPrimaryMonitor();
-    const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+    GLFWmonitor *monitor = glfwGetPrimaryMonitor();
+    const GLFWvidmode *mode = glfwGetVideoMode(monitor);
     auto xPos = mode->width / 2 - windowWidth / 2;
     auto yPos = mode->height / 2 - windowHeight / 2;
     glfwSetWindowPos(window, xPos, yPos);
@@ -109,8 +109,8 @@ PhysicalEngineLauncher::PhysicalEngineLauncher() {
     // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO();
-    (void)io;
+    ImGuiIO &io = ImGui::GetIO();
+    (void) io;
     // io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
     // io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
@@ -198,38 +198,29 @@ void PhysicalEngineLauncher::handleGui() {
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
-    if (!isFullScreen)
-    {
+    if (!isFullScreen) {
         /*------------------ImGui windows------------------*/
         {
             static bool my_tool_active = true;
             static bool showAboutPopup = false;
-            if (ImGui::BeginMainMenuBar())
-            {
-                if (ImGui::BeginMenu("File"))
-                {
+            if (ImGui::BeginMainMenuBar()) {
+                if (ImGui::BeginMenu("File")) {
                     //                    if (ImGui::MenuItem("Open..", "Ctrl+O"))
                     //                    { /* Do stuff */
                     //                    }
                     //                    if (ImGui::MenuItem("Save", "Ctrl+S"))
                     //                    { /* Do stuff */
                     //                    }
-                    if (ImGui::MenuItem("Close", "Ctrl+W"))
-                    { my_tool_active = false; }
-                    if (ImGui::MenuItem("Exit", "Alt+F4"))
-                    { glfwSetWindowShouldClose(window, true); }
+                    if (ImGui::MenuItem("Close", "Ctrl+W")) { my_tool_active = false; }
+                    if (ImGui::MenuItem("Exit", "Alt+F4")) { glfwSetWindowShouldClose(window, true); }
                     ImGui::EndMenu();
                 }
-                if (ImGui::BeginMenu("View"))
-                {
-                    if (ImGui::MenuItem("FullScreen", "F11"))
-                    { toggleFullScreen(); }
+                if (ImGui::BeginMenu("View")) {
+                    if (ImGui::MenuItem("FullScreen", "F11")) { toggleFullScreen(); }
                     ImGui::EndMenu();
                 }
-                if (ImGui::BeginMenu("Help"))
-                {
-                    if (ImGui::MenuItem("About " PROJECT_NAME "..."))
-                    {
+                if (ImGui::BeginMenu("Help")) {
+                    if (ImGui::MenuItem("About " PROJECT_NAME "...")) {
                         showAboutPopup = true;
                     }
                     ImGui::EndMenu();
@@ -239,9 +230,8 @@ void PhysicalEngineLauncher::handleGui() {
             if (showAboutPopup)
                 ImGui::OpenPopup("About " PROJECT_NAME "##AboutPopup");
             if (ImGui::BeginPopupModal("About " PROJECT_NAME "##AboutPopup", &showAboutPopup,
-                    ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoResize |
-                        ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse))
-            {
+                                       ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoResize |
+                                       ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse)) {
                 ImGui::TextColored(ImVec4(1.0f, 0.0f, 1.0f, 1.0f), PROJECT_NAME);
                 ImGui::Text("Version: %s", PROJECT_VERSION);
                 ImGui::NewLine();
@@ -250,8 +240,7 @@ void PhysicalEngineLauncher::handleGui() {
                 ImGui::NewLine();
                 ImGui::Text("Github: %s", PROJECT_GITHUB);
                 ImGui::NewLine();
-                if (ImGui::Button("Close"))
-                {
+                if (ImGui::Button("Close")) {
                     ImGui::CloseCurrentPopup();
                     showAboutPopup = false;
                 }
@@ -261,34 +250,28 @@ void PhysicalEngineLauncher::handleGui() {
         {
             ImGui::Begin("Window info");
             ImGui::Text("%.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate,
-                ImGui::GetIO().Framerate);
+                        ImGui::GetIO().Framerate);
             ImGui::Text("Window width: %d", windowWidth);
             ImGui::Text("Window height: %d", windowHeight);
             ImGui::End();
         }
         {
             ImGui::Begin("Hierarchy");
-            for (int i = 0; i < scene->getGameObjects().size(); i++)
-            {
+            for (int i = 0; i < scene->getGameObjects().size(); i++) {
                 ImGui::Selectable((scene->getPtrGameObjectByIndex(i)->getName()).c_str(),
-                    gameObject == scene->getPtrGameObjectByIndex(i));
-                if (ImGui::IsItemClicked())
-                {
+                                  gameObject == scene->getPtrGameObjectByIndex(i));
+                if (ImGui::IsItemClicked()) {
                     this->gameObject = scene->getPtrGameObjectByIndex(i);
                 }
             }
 
             ImGui::NewLine();
-            if (ImGuiUtility::ButtonCenteredOnLine("Create GameObject", 0.5f))
-            {
+            if (ImGuiUtility::ButtonCenteredOnLine("Create GameObject", 0.5f)) {
                 ImGui::OpenPopup("Create GameObject##CreateGameObjectPopup");
             }
-            if (ImGui::BeginPopup("Create GameObject##CreateGameObjectPopup"))
-            {
-                for (auto& gameObjType : Mesh::meshNamesList)
-                {
-                    if (ImGui::MenuItem(gameObjType))
-                    {
+            if (ImGui::BeginPopup("Create GameObject##CreateGameObjectPopup")) {
+                for (auto &gameObjType: Mesh::meshNamesList) {
+                    if (ImGui::MenuItem(gameObjType)) {
                         // Create and select game object in hierarchy
                         gameObject = scene->createGameObject(gameObjType);
                     }
@@ -296,8 +279,7 @@ void PhysicalEngineLauncher::handleGui() {
                 ImGui::EndPopup();
             }
 
-            if (ImGuiUtility::ButtonCenteredOnLine("Delete selected GameObject", 0.5f))
-            {
+            if (ImGuiUtility::ButtonCenteredOnLine("Delete selected GameObject", 0.5f)) {
                 scene->deleteGameObject(gameObject);
                 gameObject = nullptr;
             }
@@ -308,8 +290,10 @@ void PhysicalEngineLauncher::handleGui() {
             ImGui::Begin("View tools");
             ImGui::Text("Background color");
             ImGui::ColorPicker4("Background color", backgroundColor.data());
+#ifndef __EMSCRIPTEN__
             ImGui::Checkbox("Mesh: Fill/Line", scene->getPtrWireFrameState());
-            ImGui::Checkbox("Show axis", scene->getPtrShowAxis());
+#endif
+//            ImGui::Checkbox("Show axis", scene->getPtrShowAxis());
             ImGui::End();
         }
         {
@@ -332,27 +316,24 @@ void PhysicalEngineLauncher::handleGui() {
             //                consoleBuffer.fill('\0');
             //            }
             //            ImGui::End();
-        } {
+        }
+        {
             ImGui::Begin("Speed graph viewer");
             {
                 static RollingBuffer rdata1, rdata2, rdata3;
                 static float t = 0;
                 t += ImGui::GetIO().DeltaTime;
 
-                PhysicalComponent* pPhysicalComponent = nullptr;
-                if (gameObject != nullptr)
-                {
+                PhysicalComponent *pPhysicalComponent = nullptr;
+                if (gameObject != nullptr) {
                     gameObject->getComponentByClass(pPhysicalComponent);
                 }
-                if (pPhysicalComponent != nullptr)
-                {
+                if (pPhysicalComponent != nullptr) {
                     Vector3d speed = pPhysicalComponent->getLinearSpeed();
                     rdata1.AddPoint(t, speed.x);
                     rdata2.AddPoint(t, speed.y);
                     rdata3.AddPoint(t, speed.z);
-                }
-                else
-                {
+                } else {
                     rdata1.AddPoint(t, 0);
                     rdata2.AddPoint(t, 0);
                     rdata3.AddPoint(t, 0);
@@ -361,20 +342,19 @@ void PhysicalEngineLauncher::handleGui() {
                 static float history = 10.0f;
                 static ImPlotAxisFlags flags = ImPlotAxisFlags_NoTickLabels;
 
-                if (ImPlot::BeginPlot("GameObject Speed##Rolling", ImVec2(-1, 150)))
-                {
+                if (ImPlot::BeginPlot("GameObject Speed##Rolling", ImVec2(-1, 150))) {
                     ImPlot::SetupAxes(nullptr, nullptr, flags, flags);
                     ImPlot::SetupAxisLimits(ImAxis_X1, 0, history, ImGuiCond_Always);
                     ImPlot::SetupAxisLimits(ImAxis_Y1, 0, 1);
                     ImPlot::PlotLine("Speed X##ImPlotSpeedX", &rdata1.Data[0].x, &rdata1.Data[0].y, rdata1.Data.size(),
-                        0, 0,
-                        2 * sizeof(float));
+                                     0, 0,
+                                     2 * sizeof(float));
                     ImPlot::PlotLine("Speed Y##ImPlotSpeedY", &rdata2.Data[0].x, &rdata2.Data[0].y, rdata2.Data.size(),
-                        0, 0,
-                        2 * sizeof(float));
+                                     0, 0,
+                                     2 * sizeof(float));
                     ImPlot::PlotLine("Speed Z##ImPlotSpeedZ", &rdata3.Data[0].x, &rdata3.Data[0].y, rdata3.Data.size(),
-                        0, 0,
-                        2 * sizeof(float));
+                                     0, 0,
+                                     2 * sizeof(float));
                     ImPlot::EndPlot();
                 }
                 ImGui::SliderFloat("History", &history, 1, 30, "%.1f s");
@@ -384,51 +364,38 @@ void PhysicalEngineLauncher::handleGui() {
         }
         {
             ImGui::Begin("Inspector");
-            if (gameObject != nullptr)
-            {
+            if (gameObject != nullptr) {
                 ImGui::Text("Name: %s", gameObject->getName().c_str());
-                if (ImGui::CollapsingHeader("Transform", ImGuiTreeNodeFlags_DefaultOpen))
-                {
+                if (ImGui::CollapsingHeader("Transform", ImGuiTreeNodeFlags_DefaultOpen)) {
                     gameObject->drawTransformGui();
                 }
-                if (ImGui::CollapsingHeader("Mesh", ImGuiTreeNodeFlags_DefaultOpen))
-                {
+                if (ImGui::CollapsingHeader("Mesh", ImGuiTreeNodeFlags_DefaultOpen)) {
                     gameObject->drawMeshGui();
                 }
-                for (Component* component : gameObject->getComponents())
-                {
-                    if (ImGui::CollapsingHeader(component->getName().c_str(), ImGuiTreeNodeFlags_DefaultOpen))
-                    {
+                for (Component *component: gameObject->getComponents()) {
+                    if (ImGui::CollapsingHeader(component->getName().c_str(), ImGuiTreeNodeFlags_DefaultOpen)) {
                         component->drawGui();
                     }
                 }
                 ImGui::NewLine();
-                if (ImGuiUtility::ButtonCenteredOnLine("Add component", 0.5f))
-                {
+                if (ImGuiUtility::ButtonCenteredOnLine("Add component", 0.5f)) {
                     ImGui::OpenPopup("Add component##AddComponentPopup");
                 }
-                if (ImGui::BeginPopup("Add component##AddComponentPopup"))
-                {
-                    for (auto& componentName : Component::componentsNamesList)
-                    {
-                        if (ImGui::MenuItem(componentName))
-                        {
+                if (ImGui::BeginPopup("Add component##AddComponentPopup")) {
+                    for (auto &componentName: Component::componentsNamesList) {
+                        if (ImGui::MenuItem(componentName)) {
                             gameObject->addComponentByName(componentName);
                         }
                     }
                     ImGui::EndPopup();
                 }
                 ImGui::NewLine();
-                if (ImGuiUtility::ButtonCenteredOnLine("Delete component", 0.5f))
-                {
+                if (ImGuiUtility::ButtonCenteredOnLine("Delete component", 0.5f)) {
                     ImGui::OpenPopup("Delete component##DeleteComponentPopup");
                 }
-                if (ImGui::BeginPopup("Delete component##DeleteComponentPopup"))
-                {
-                    for (auto& component : gameObject->getComponents())
-                    {
-                        if (ImGui::MenuItem(component->getName().c_str()))
-                        {
+                if (ImGui::BeginPopup("Delete component##DeleteComponentPopup")) {
+                    for (auto &component: gameObject->getComponents()) {
+                        if (ImGui::MenuItem(component->getName().c_str())) {
                             gameObject->deleteComponentByName(component->getName());
                         }
                     }
@@ -441,17 +408,19 @@ void PhysicalEngineLauncher::handleGui() {
             //            ImGui::Begin("Project files");
             //
             //            ImGui::End();
-        } {
+        }
+        {
             //            ImGui::Begin("Game settings");
             //            ImGui::Text("Particle speed:");
             //            ImGui::InputFloat("##PhysicalEngineParticleSpeed", game.getPtrSpeed(), 0.1f, 1.0f, "%.1f");
             //            ImGui::End();
-        } {
+        }
+        {
             ImGui::Begin("Scene View", nullptr, ImGuiWindowFlags_NoCollapse);
             {
                 ImGui::BeginChild("GameRender");
                 ImVec2 windowSize = ImGui::GetWindowSize();
-                ImGui::Image((ImTextureID)scene->getFrameBufferId(), windowSize, ImVec2(0, 1), ImVec2(1, 0));
+                ImGui::Image((ImTextureID) scene->getFrameBufferId(), windowSize, ImVec2(0, 1), ImVec2(1, 0));
                 ImGui::EndChild();
             }
             ImGui::End();
@@ -460,12 +429,12 @@ void PhysicalEngineLauncher::handleGui() {
     ImGui::Render();
 }
 
-void PhysicalEngineLauncher::updateGame(std::chrono::steady_clock::time_point& start) {
+void PhysicalEngineLauncher::updateGame(std::chrono::steady_clock::time_point &start) {
     auto deltaTime = std::chrono::duration_cast<std::chrono::milliseconds>(
-        std::chrono::steady_clock::now() - start)
-                         .count();
+            std::chrono::steady_clock::now() - start)
+            .count();
     start = std::chrono::steady_clock::now();
-    scene->update((float)deltaTime / 1000.0f);
+    scene->update((float) deltaTime / 1000.0f);
     //    scene->update(1000.0f / ImGui::GetIO().Framerate);
 }
 
@@ -476,15 +445,13 @@ void PhysicalEngineLauncher::updateScreen() {
     updateViewport(display_w, display_h);
 
     // If window is minimized, don't draw anything (to avoid draw in a 0x0 window)
-    if (!isMinimized())
-    {
+    if (!isMinimized()) {
 
         // Clear Main Screen
         clearScreen();
 
         // If not in fullscreen, clear the opengl window
-        if (!isFullScreen)
-        {
+        if (!isFullScreen) {
             glBindFramebuffer(GL_FRAMEBUFFER, scene->getFrameBufferId());
             clearScreen();
         }
@@ -506,8 +473,7 @@ void PhysicalEngineLauncher::clearScreen() {
 void PhysicalEngineLauncher::updateViewport(int width, int height) {
     glViewport(0, 0, width, height);
     scene->updateViewport(width, height);
-    if (!isFullScreen)
-    {
+    if (!isFullScreen) {
         windowWidth = width;
         windowHeight = height;
     }
@@ -515,18 +481,16 @@ void PhysicalEngineLauncher::updateViewport(int width, int height) {
 
 
 void PhysicalEngineLauncher::toggleFullScreen() {
+    std::cout << "Toggle fullscreen" << std::endl;
 #ifndef __EMSCRIPTEN__
-    GLFWmonitor* monitor = glfwGetPrimaryMonitor();
-    const GLFWvidmode* mode = glfwGetVideoMode(monitor);
-    if (isFullScreen)
-    {
+    GLFWmonitor *monitor = glfwGetPrimaryMonitor();
+    const GLFWvidmode *mode = glfwGetVideoMode(monitor);
+    if (isFullScreen) {
         auto xPos = mode->width / 2 - windowWidth / 2;
         auto yPos = mode->height / 2 - windowHeight / 2;
         glfwSetWindowMonitor(window, nullptr, xPos, yPos, windowWidth, windowHeight, 0);
         isFullScreen = false;
-    }
-    else
-    {
+    } else {
         glfwSetWindowMonitor(window, monitor, 0, 0, mode->width, mode->height, mode->refreshRate);
         isFullScreen = true;
     }
@@ -534,7 +498,7 @@ void PhysicalEngineLauncher::toggleFullScreen() {
 }
 
 bool PhysicalEngineLauncher::isMinimized() const {
-    return (windowWidth == 0 && windowHeight == 0);
+    return (windowWidth == 0 || windowHeight == 0);
 }
 
 // void PhysicalEngineLauncher::focusCameraOnGameObject() {
